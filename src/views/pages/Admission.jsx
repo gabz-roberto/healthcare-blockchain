2e052f233bc740d2794f3e7295d9bd550ebfd771
Gabriel Roberto <gabrielrobertoal@gmail.com>
2021-12-18 08:52:01 -0300

    admissao

--- src/views/pages/Admission.jsx
+++ src/views/pages/Admission.jsx
@@ -129,6 +129,7 @@ const Admission = (props) => {
             </div>
           </div>
         </section>
+
         <section className="theme-section">
             <h3>Localização prévia do paciente</h3>
             <div className="inner-section">
@@ -141,8 +142,140 @@ const Admission = (props) => {
                     <label htmlFor="idUniversalInstalacao">ID universal: </label>
                     <input type="text" placeholder="Digite aqui..." id="idUniversalInstalacao" />
                 </div>
+                <div className="box">
+                  <label htmlFor="idUniversalTipo">Tipo do ID Universal: </label>
+                  <select name="idUniversalTipo" id="idUniversalTipo">
+                    <option value="DNS">DNS</option>
+                    <option value="GUID">GUID</option>
+                    <option value="HCD">HCD</option>
+                    <option value="HL7">HL7</option>
+                    <option value="ISO">ISO</option>
+                    <option value="LMN">L, M, N</option>
+                    <option value="random">Random</option>
+                    <option value="UUID">UUID</option>
+                    <option value="x400">x400</option>
+                    <option value="x500">x500</option>
+                  </select>
+                </div>
+            </div>
+            <div className="inner-section">
+              <h4>Endereço</h4>
+              <div className="box">
+                <label htmlFor="pacienteLogradouro">Logradouro: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteLogradouro" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteCidade">Cidade: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteCidade" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteEstado">Estado: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteEstado" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteCep">CEP: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteCep" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacientePais">País: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacientePais" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteUnidade">Unidade de cuidado: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteUnidade" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteAla">Ala: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteAla" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteQuarto">Quarto: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteQuarto" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteLeito">Leito: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteLeito" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacientePredio">Predio: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacientePredio" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteAndar">Andar: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteAndar" />
+              </div>
+              <div className="box">
+                <label htmlFor="pacienteDescricaoLocal">Descrição da localização: </label>
+                <input type="text" placeholder="Digite aqui..." id="pacienteDescricaoLocal" />
+              </div>
             </div>
         </section>
+
+        <section className="theme-section">
+          <h3>Médico do atendimento</h3>
+          <div className="inner-section">
+            <div className="box">
+              <label htmlFor="medicoAtendimentoId">ID: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAtendimentoId" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoAtendimentoNome">Nome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAtendimentoNome" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoAtendimentoSobrenome">Sobrenome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAtendimentoSobrenome" />
+            </div>
+          </div>
+        </section>
+
+        <section className="theme-section">
+          <h3>Médico solicitante</h3>
+          <div className="inner-section">
+            <div className="box">
+              <label htmlFor="medicoSolicitanteId">ID: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoSolicitanteId" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoSolicitanteNome">Nome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoSolicitanteNome" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoSolicitanteSobrenome">Sobrenome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoSolicitanteSobrenome" />
+            </div>
+          </div>
+        </section>
+
+        <section className="theme-section">
+          <h3>Médico especialista</h3>
+          <div className="inner-section">
+            <div className="box">
+              <label htmlFor="medicoEspecialistaId">ID: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoEspecialistaId" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoEspecialistaNome">Nome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoEspecialistaNome" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoEspecialistaSobrenome">Sobrenome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoEspecialistaSobrenome" />
+            </div>
+          </div>
+          <div className="inner-section">
+            <div className="box">
+              <label htmlFor="medicoEspecialistaUnidade">Unidade hospitalar: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoEspecialistaUnidade" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoEspecialistaFonte">Fonte da admissão: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoEspecialistaFonte" />
+            </div>
+          </div>
+        </section>
+
+        
       </form>
     </div>
   );
