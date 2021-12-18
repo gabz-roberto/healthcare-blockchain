e53ba4a71216ad98f447e21c0159ee7560716723
Gabriel Roberto <gabrielrobertoal@gmail.com>
2021-12-18 19:57:54 -0300

    admissao

--- src/views/pages/Admission.jsx
+++ src/views/pages/Admission.jsx
@@ -275,7 +275,47 @@ const Admission = (props) => {
           </div>
         </section>
 
-        
+        <section className="theme-section">
+          <h3>Médico da admissão</h3>
+          <div className="inner-section">
+            <div className="box">
+              <label htmlFor="medicoAdmissaoId">ID: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAdmissaoId" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoAdmissaoNome">Nome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAdmissaoNome" />
+            </div>
+            <div className="box">
+              <label htmlFor="medicoAdmissaoSobrenome">Sobrenome: </label>
+              <input type="text" placeholder="Digite aqui..." id="medicoAdmissaoSobrenome" />
+            </div>
+          </div>
+        </section>
+
+        <section className="theme-section">
+          <label htmlFor="tipoFinanciamento">Tipo de financiamento: </label>
+          <select name="tipoFinanciamento" id="tipoFinanciamento">
+            <option value="ausHealth">*Australian Health Care Agreements(en)</option>
+            <option value="seguroPrivado">Seguros de saúde privados</option>
+            <option value="autoFinanciado">Auto financiado</option>
+            <option value="auxilioDoenca">Auxilio doença</option>
+            <option value="reclamacaoTerceiro">Reclamação de terceiro sobre veículo motorizado</option>
+            <option value="outraCompensacao">Outra compensação</option>
+            <option value="depVeterans">*Department of Veterans' Affairs(en)</option>
+            <option value="depDefence">*Department of Defence(en)</option>
+            <option value="corFacility">*Correctional facility(en)</option>
+            <option value="outroHospital">Outro hospital ou instituição pública (cuidados contratados)</option>         
+            <option value="acordosReciprocos">Acordos de saúde recíprocos (com outros países)</option>
+            <option value="outro">Outro</option>
+            <option value="desconhecido">Desconhecido</option>
+          </select>
+        </section>
+
+        <section className="theme-section">
+          <label htmlFor="dataAdmissao">Data de admissão: </label>
+          <input type="date" name="dataAdmissao" id="dataAdmissao" />
+        </section>
       </form>
     </div>
   );
