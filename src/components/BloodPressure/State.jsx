2581f233b5c2d7aa009389e150e63959c8e1c4b2
Gabriel Roberto <gabrielrobertoal@gmail.com>
2021-12-22 18:17:46 -0300

    estado

--- src/components/BloodPressure/State.jsx
+++ src/components/BloodPressure/State.jsx
@@ -13,6 +13,19 @@ const State = (props) => {
                     <option value="inclinadoEsqueda">Deitado com inclinação para esquerda</option>
                 </select>
             </div>
+
+            <div className="box">
+                <label htmlFor="fatoreConfundidores">Fatores confundidores: </label>
+                <input type="text" id="fatoresConfundidores" name="fatoresConfundidores"/>
+            </div>
+
+            <div className="box">
+                <label htmlFor="statusSono">Status do sono: </label>
+                <select name="statusSono" id="statusSono">
+                    <option value="alertaAcordado">Alerta e acordado</option>
+                    <option value="dormindo">Dormindo</option>
+                </select>
+            </div>
         </section>
     )
 }
